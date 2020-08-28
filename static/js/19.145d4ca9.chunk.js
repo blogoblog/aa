b1d6484e9004a71060fc356eab74be866c61053f
blogoblog <70345558+blogoblog@users.noreply.github.com>
2020-08-28 17:34:18 +0530

    ok

--- static/js/19.145d4ca9.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpsynthetix.exchange"]=this["webpackJsonpsynthetix.exchange"]||[]).push([[19],{902:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return l}));var a=t(0),c=t.n(a),r=t(362),u=t(271),l=function(){return c.a.createElement(c.a.Fragment,null,c.a.createElement(r.a,null),c.a.createElement(u.a,null))}}}]);
-//# sourceMappingURL=19.145d4ca9.chunk.js.map
